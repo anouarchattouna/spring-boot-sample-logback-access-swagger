5f9606a645a81f1e79594e53ab23cc743531f3fe
Anouar Chattouna <anouar.chattouna-ext@socgen.com>
2016-11-10 14:24:37 +0100

    Add a new operation using the sample service

--- src/main/java/org/anotherdev/sample/web/rest/v1/SampleController.java
+++ src/main/java/org/anotherdev/sample/web/rest/v1/SampleController.java
@@ -5,8 +5,10 @@ import java.time.OffsetDateTime;
 import javax.servlet.http.HttpServletRequest;
 
 import org.anotherdev.sample.domain.Sample;
+import org.anotherdev.sample.service.SampleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -27,9 +29,19 @@ public class SampleController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SampleController.class);
 
+  @Autowired
+  private SampleService sampleService;
+
   @RequestMapping(value = "/ping", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Sample> ping(HttpServletRequest request) {
     Sample sample = new Sample(request.getLocale().getISO3Country(), OffsetDateTime.now().toString());
+    LOGGER.info(sample.toString());
+    return new ResponseEntity<Sample>(sample, HttpStatus.OK);
+  }
+
+  @RequestMapping(value = "/ping1", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Sample> ping1() {
+    Sample sample = new Sample(sampleService.getClientCountry(), OffsetDateTime.now().toString());
     LOGGER.info(sample.toString());
     return new ResponseEntity<Sample>(sample, HttpStatus.OK);
   }
