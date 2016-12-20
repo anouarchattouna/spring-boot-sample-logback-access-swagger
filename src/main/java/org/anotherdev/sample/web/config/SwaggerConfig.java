5f8e9d4543cfbae3bca893d0f1fa4e4481d11c52
Anouar Chattouna <anouar.chattouna-ext@socgen.com>
2016-12-20 09:33:56 +0100

    Update API documentation

--- src/main/java/org/anotherdev/sample/web/config/SwaggerConfig.java
+++ src/main/java/org/anotherdev/sample/web/config/SwaggerConfig.java
@@ -24,6 +24,14 @@ public class SwaggerConfig {
   @Value("${application.version}")
   private String applicationVersion;
 
+  private static final String API_DESCRIPTION = "Lorem ipsum dolor sit amet, et mi eu, ipsum taciti. Placerat commodo. Suscipit nibh urna, mauris erat sed, accumsan tincidunt sit. Lorem orci enim. Mi quis, nibh quam, eleifend elit.</p>"
+    + "Sed morbi, vel commodo. Accumsan ligula, odio sollicitudin vel, non dolorem. Nullam sapien commodo, ipsum amet congue. Posuere vitae lacus, turpis eros. A felis.</p>"
+    + "A volutpat, viverra nullam a. Per dignissim, velit suspendisse. Vulputate justo, lacinia neque. <strong>Magna class, est ac.</strong> Lobortis dapibus iaculis. Felis erat.</p>"
+    + "Some useful link <a href=\"https://github.com/springfox/springfox\">Springfox</a>."
+    + "<h3 id=\"security\">Security</h3>"
+    + "oauth (OAuth 2.0)</p>"
+    + "<ul><li>Example :<pre><code>Authorization: Bearer &lt;domain-token-value&gt;</code></pre></li></ul>";
+
   // @formatter:off
     @Bean
     public Docket sampleApi() {
@@ -39,7 +47,7 @@ public class SwaggerConfig {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Sample REST API")
-                .description("This is a sample REST API")
+                .description(API_DESCRIPTION)
                 .version(applicationVersion)
                 .build();
     }
